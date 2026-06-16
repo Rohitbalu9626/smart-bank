5107b204dbee1a683faabef1007c8ae96123d8c4
Rohitbalu9626 <baluofficial33@gmail.com>
2026-06-16 22:23:20 +0530

    Simplify BankSystemApplication class
    Removed unnecessary imports and excluded configuration.

--- src/main/java/com/bank/system/BankSystemApplication.java
+++ src/main/java/com/bank/system/BankSystemApplication.java
@@ -2,11 +2,8 @@ package com.bank.system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.reactive.ReactiveUserDetailsServiceAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(exclude = { ReactiveUserDetailsServiceAutoConfiguration.class }) // 👈 FORCE EXCLUDE!
-@ComponentScan(basePackages = "com.bank.system")
+@SpringBootApplication
 public class BankSystemApplication {
 
     public static void main(String[] args) {
